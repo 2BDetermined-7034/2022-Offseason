c744495a29693e96df52d2a2dd1256ebbc3be84b
Team7034 <33676445+Team7034@users.noreply.github.com>
2022-11-29 20:22:32 -0800

    Create Camera.java

---
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.PhotonCamera;
+import static Constants.camera.*;
+
+public class Camera extends SubsystemBase {
+
+  PhotonCamera camera;
+
+
+
+
+
+
+
+  /** Creates a new Camera. */
+  public Camera(PhotonCamera camera) {
+    this.camera = camera;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+
+}
